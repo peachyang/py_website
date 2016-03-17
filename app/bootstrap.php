369ecad589f4008e3c32e90919011c9a76fee423
denis.chung <denis.chung@DESKTOP-GQFI6PJ>
2016-03-17 17:59:21 +0800

    file system

---
+++ app/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+umask(0);
+
+if (version_compare(phpversion(), '5.5.0', '<') === true) {
+    echo 'SeaHiNet E-Commerce Suite v2 supports PHP 5.5.0 or later.(Current: ', phpversion(), ')';
+    exit(1);
+}
+
+define('DS', DIRECTORY_SEPARATOR);
+define('BP', dirname(__DIR__) . DS);
+
+require BP . 'vendor/autoload.php';
+
+date_default_timezone_set('UTC');

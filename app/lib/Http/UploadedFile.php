cab9d6de33b70c950146fd1c44025d1414e243a7
denis.chung <denis.chung@DESKTOP-GQFI6PJ>
2016-03-22 09:33:32 +0800

    document

--- app/lib/Http/UploadedFile.php
+++ app/lib/Http/UploadedFile.php
@@ -14,60 +14,42 @@ class UploadedFile implements UploadedFileInterface
 {
 
     /**
-     * The client-provided file name.
-     *
      * @var string
      */
     protected $name;
 
     /**
-     * The client-provided media type of the file.
-     *
      * @var string
      */
     protected $type;
 
     /**
-     * The size of the file in bytes.
-     *
      * @var int
      */
     protected $size;
 
     /**
-     * A valid PHP UPLOAD_ERR_xxx code for the file upload.
-     *
      * @var int
      */
     protected $error = UPLOAD_ERR_OK;
 
     /**
-     * Indicates if the upload is from a SAPI environment.
-     *
      * @var bool
      */
     protected $sapi = false;
 
     /**
-     * An optional StreamInterface wrapping the file resource.
-     *
      * @var StreamInterface
      */
     protected $stream;
 
     /**
-     * Indicates if the uploaded file has already been moved.
-     *
      * @var bool
      */
     protected $moved = false;
 
     /**
-     * Create a normalized tree of UploadedFile instances from the Environment.
-     *
-     * @param Environment $env The environment
-     *
-     * @return array|null A normalized tree of UploadedFile instances or null if none are provided.
+     * @return array
      */
     public static function createFromEnvironment()
     {
@@ -79,11 +61,8 @@ class UploadedFile implements UploadedFileInterface
     }
 
     /**
-     * Parse a non-normalized, i.e. $_FILES superglobal, tree of uploaded file data.
-     *
-     * @param array $uploadedFiles The non-normalized tree of uploaded file data.
-     *
-     * @return array A normalized tree of UploadedFile instances.
+     * @param array $uploadedFiles
+     * @return array
      */
     private static function parseUploadedFiles(array $uploadedFiles)
     {
@@ -113,14 +92,12 @@ class UploadedFile implements UploadedFileInterface
     }
 
     /**
-     * Construct a new UploadedFile instance.
-     *
-     * @param string      $file The full path to the uploaded file provided by the client.
-     * @param string|null $name The file name.
-     * @param string|null $type The file media type.
-     * @param int|null    $size The file size in bytes.
-     * @param int         $error The UPLOAD_ERR_XXX code representing the status of the upload.
-     * @param bool        $sapi Indicates if the upload is in a SAPI environment.
+     * @param string      $file
+     * @param string|null $name
+     * @param string|null $type
+     * @param int|null    $size
+     * @param int         $error
+     * @param bool        $sapi
      */
     public function __construct($file, $name = null, $type = null, $size = null, $error = UPLOAD_ERR_OK, $sapi = false)
     {
@@ -133,20 +110,8 @@ class UploadedFile implements UploadedFileInterface
     }
 
     /**
-     * Retrieve a stream representing the uploaded file.
-     *
-     * This method MUST return a StreamInterface instance, representing the
-     * uploaded file. The purpose of this method is to allow utilizing native PHP
-     * stream functionality to manipulate the file upload, such as
-     * stream_copy_to_stream() (though the result will need to be decorated in a
-     * native PHP stream wrapper to work with such functions).
-     *
-     * If the moveTo() method has been called previously, this method MUST raise
-     * an exception.
-     *
-     * @return StreamInterface Stream representation of the uploaded file.
-     * @throws \RuntimeException in cases when no stream is available or can be
-     *     created.
+     * @return StreamInterface
+     * @throws RuntimeException
      */
     public function getStream()
     {
@@ -161,38 +126,11 @@ class UploadedFile implements UploadedFileInterface
     }
 
     /**
-     * Move the uploaded file to a new location.
-     *
-     * Use this method as an alternative to move_uploaded_file(). This method is
-     * guaranteed to work in both SAPI and non-SAPI environments.
-     * Implementations must determine which environment they are in, and use the
-     * appropriate method (move_uploaded_file(), rename(), or a stream
-     * operation) to perform the operation.
-     *
-     * $targetPath may be an absolute path, or a relative path. If it is a
-     * relative path, resolution should be the same as used by PHP's rename()
-     * function.
-     *
-     * The original file or stream MUST be removed on completion.
-     *
-     * If this method is called more than once, any subsequent calls MUST raise
-     * an exception.
-     *
-     * When used in an SAPI environment where $_FILES is populated, when writing
-     * files via moveTo(), is_uploaded_file() and move_uploaded_file() SHOULD be
-     * used to ensure permissions and upload status are verified correctly.
-     *
-     * If you wish to move to a stream, use getStream(), as SAPI operations
-     * cannot guarantee writing to stream destinations.
-     *
      * @see http://php.net/is_uploaded_file
      * @see http://php.net/move_uploaded_file
-     *
-     * @param string $targetPath Path to which to move the uploaded file.
-     *
-     * @throws InvalidArgumentException if the $path specified is invalid.
-     * @throws RuntimeException on any error during the move operation, or on
-     *     the second or subsequent call to the method.
+     * @param string $targetPath
+     * @throws InvalidArgumentException
+     * @throws RuntimeException
      */
     public function moveTo($targetPath)
     {
@@ -230,19 +168,8 @@ class UploadedFile implements UploadedFileInterface
     }
 
     /**
-     * Retrieve the error associated with the uploaded file.
-     *
-     * The return value MUST be one of PHP's UPLOAD_ERR_XXX constants.
-     *
-     * If the file was uploaded successfully, this method MUST return
-     * UPLOAD_ERR_OK.
-     *
-     * Implementations SHOULD return the value stored in the "error" key of
-     * the file in the $_FILES array.
-     *
      * @see http://php.net/manual/en/features.file-upload.errors.php
-     *
-     * @return int One of PHP's UPLOAD_ERR_XXX constants.
+     * @return int
      */
     public function getError()
     {
@@ -250,17 +177,7 @@ class UploadedFile implements UploadedFileInterface
     }
 
     /**
-     * Retrieve the filename sent by the client.
-     *
-     * Do not trust the value returned by this method. A client could send
-     * a malicious filename with the intention to corrupt or hack your
-     * application.
-     *
-     * Implementations SHOULD return the value stored in the "name" key of
-     * the file in the $_FILES array.
-     *
-     * @return string|null The filename sent by the client or null if none
-     *     was provided.
+     * @return string|null
      */
     public function getClientFilename()
     {
@@ -268,17 +185,7 @@ class UploadedFile implements UploadedFileInterface
     }
 
     /**
-     * Retrieve the media type sent by the client.
-     *
-     * Do not trust the value returned by this method. A client could send
-     * a malicious media type with the intention to corrupt or hack your
-     * application.
-     *
-     * Implementations SHOULD return the value stored in the "type" key of
-     * the file in the $_FILES array.
-     *
-     * @return string|null The media type sent by the client or null if none
-     *     was provided.
+     * @return string|null
      */
     public function getClientMediaType()
     {
@@ -286,13 +193,7 @@ class UploadedFile implements UploadedFileInterface
     }
 
     /**
-     * Retrieve the file size.
-     *
-     * Implementations SHOULD return the value stored in the "size" key of
-     * the file in the $_FILES array if available, as PHP calculates this based
-     * on the actual size transmitted.
-     *
-     * @return int|null The file size in bytes or null if unknown.
+     * @return int|null
      */
     public function getSize()
     {

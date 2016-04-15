47d41e06f21313d5c1f66c3e056b2a3b788e47a8
root <root@localhost.localdomain>
2016-04-15 16:24:49 +0800

    merge

---
+++ app/code/Cms/Testers/Unit/Controller/PageTester.php
@@ -0,0 +1,19 @@
+<?php
+namespace Seahinet\Cms\Testers\Unit\Controller\Page;
+
+class PageTester  extends \PHPUnit_Framework_TestCase{
+	
+	protected function setUp()
+	{
+		
+		
+		
+	}
+	
+	
+	
+}
+
+
+
+?>

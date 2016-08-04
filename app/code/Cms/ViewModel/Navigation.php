d3de6cd15c197eff5c47104c0dd1febaa0b55948
peach <peach@DESKTOP-RD2AI69>
2016-08-04 14:17:33 +0800

    Merge origin/master
    Conflicts:
    	app/tpl/default/catalog/product/view.phtml

--- app/code/Cms/ViewModel/Navigation.php
+++ app/code/Cms/ViewModel/Navigation.php
@@ -2,21 +2,36 @@
 
 namespace Seahinet\Cms\ViewModel;
 
+use Seahinet\Cms\Model\Category;
 use Seahinet\Lib\ViewModel\Template;
 
 class Navigation extends Template
 {
 
-    protected $navigationModel = null;
+    protected $navigations = null;
+    protected $urls = [];
 
-    public function getNavigationModel()
+    public function getTemplate()
     {
-        return $this->navigationModel;
+        if (!$this->template) {
+            return 'cms/main' . $this->getQuery();
+        }
+        return parent::getTemplate();
     }
 
-    public function setNavigationModel()
+    public function getCategory()
     {
-        $this->navigationModel = $navigationModel;
+        return $this->getVariable('category');
+    }
+
+    public function getNavigation()
+    {
+        return $this->navigations;
+    }
+
+    public function setNavigation($navigations)
+    {
+        $this->navigations = $navigations;
         return $this;
     }
 

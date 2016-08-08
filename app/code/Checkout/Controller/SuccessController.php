3f27be080486e05b22fa10e3a2e2ba9763d1fbf4
peach <peach@DESKTOP-RD2AI69>
2016-08-08 16:10:16 +0800

    Merge origin/master
    Conflicts:
    	app/tpl/default/catalog/product/view.phtml

--- app/code/Checkout/Controller/SuccessController.php
+++ app/code/Checkout/Controller/SuccessController.php
@@ -3,13 +3,17 @@
 namespace Seahinet\Checkout\Controller;
 
 use Seahinet\Lib\Controller\ActionController;
+use Seahinet\Sales\Model\Cart;
 
 class SuccessController extends ActionController
 {
 
     public function indexAction()
     {
-        exit;
+        if (count(Cart::instance()->getItems())) {
+            return $this->getLayout('checkout_order_success');
+        }
+        return $this->redirectReferer('checkout/cart/');
     }
 
 }

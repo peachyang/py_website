2051a024ddb98fa26e7e20e92d79dddc028f7291
Chase <chasegeng@seahinet.com>
2016-08-17 16:49:45 +0800

    login

--- app/code/Catalog/Controller/ReviewController.php
+++ app/code/Catalog/Controller/ReviewController.php
@@ -6,6 +6,7 @@ use Seahinet\Lib\Bootstrap;
 use Seahinet\Lib\Controller\ActionController;
 use Seahinet\Catalog\Model\Product\Review;
 use TijsVerkoyen\Akismet\Exception;
+use Seahinet\Lib\Session\Segment;
 
 class ReviewController extends ActionController
 {
@@ -13,19 +14,30 @@ class ReviewController extends ActionController
     public function saveAction()
     {
         if ($this->getRequest()->isPost()) {
+            $segment = new Segment('customer');
+            if (!$this->getContainer()->get('config')['catalog/review/guests'] && !$segment->get('hasLoggedIn')){
+                return $this->notFoundAction();
+            }
             $data = $this->getRequest()->getPost();
-            $data['status'] = 1;
+            $data['status'] = (int)$this->getContainer()->get('config')['catalog/review/status'];
             $data['language_id'] = Bootstrap::getLanguage()->getId();
             $review = new Review($data);
             try {
                 if (!$this->getContainer()->get('akismet')->isSpam($data['content'])) {
+                    $result['error'] = 0;
+                    $result['message'][] = ['message' => $this->translate('Submitted Successfully'), 'level' => 'success'];
                     $review->save();
                 }
             } catch (Exception $e) {
+                $result['error'] = 1;
+                $result['message'][] = ['message' => $this->translate('An error detected. Please contact us or try again later.'), 'level' => 'danger'];
                 $review->setData('status', 0)->save();
             }
-            echo"<script>alert('" . $this->translate('Submitted Successfully') . "');history.go(-1);</script>";
+        }else{
+            $result['error'] = 1;
+            $result['message'][] = ['message' => $this->translate('An error detected. Please contact us or try again later.'), 'level' => 'danger'];
         }
+        return $this->response(isset($result)?$result:['error'=>0,'message'=>[]], $this->getRequest()->getHeader('HTTP_REFERER'), 'catalog');
     }
 
 }
--- app/code/I18n/Controller/LocateController.php
+++ app/code/I18n/Controller/LocateController.php
@@ -15,6 +15,7 @@ class LocateController extends ActionController
         $locate = new Locate;
         $locale = Bootstrap::getLanguage()->offsetGet('code');
         $result = [];
+        $code = $this->getContainer()->get('geoip')->get($_SERVER['REMOTE_ADDR'])['country']['iso_code'];
         if ($data) {
             foreach ($data as $part => $id) {
                 $resultSet = $locate->load($part, $id);
@@ -24,16 +25,27 @@ class LocateController extends ActionController
             $resultSet = $locate->load('country');
         }
         foreach ($resultSet as $id => $item) {
-            $result[] = [
-                'value' => $id,
-                'code' => isset($item['iso2_code']) ? $item['iso2_code'] : $item['code'],
-                'label' => $item->getName($locale)
-            ];
+            if(isset($item['iso2_code'])&&$item['iso2_code']===$code){
+                $default = [
+                    'value' => $id,
+                    'code' => $code,
+                    'label' => $item->getName($locale)
+                ];
+            }else{
+                $result[] = [
+                    'value' => $id,
+                    'code' => isset($item['iso2_code']) ? $item['iso2_code'] : $item['code'],
+                    'label' => $item->getName($locale)
+                ];
+            }
         }
         uasort($result, function($a, $b) {
             $result = strnatcmp($a['code'], $b['code']);
             return $result > 0 ? 1 : ($result < 0 ? -1 : 0);
         });
+        if(isset($default)){
+            array_unshift($result, $default);
+        }
         return array_values($result);
     }
 

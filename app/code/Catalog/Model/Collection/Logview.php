8ac2663d0ddd95719266ecf7898dbeec503dd1dc
peach <peach@DESKTOP-RD2AI69>
2016-08-17 12:06:00 +0800

    Merge origin/master
    Conflicts:
    	app/i18n/zh-CN/checkout.csv

--- app/code/Catalog/Model/Collection/Logview.php
+++ app/code/Catalog/Model/Collection/Logview.php
@@ -9,7 +9,7 @@ class Logview extends AbstractCollection
 
     protected function construct()
     {
-        $this->init('log_view', 'customer_id,product_id', ['customer_id', 'product_id', 'created_at', 'updated_at']);
+        $this->init('log_view');
     }
 
 }

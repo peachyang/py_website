3d7afe8c21078d5a94f3f94cf26c69b1cc309c86
peach <peach@DESKTOP-RD2AI69>
2016-08-19 14:37:33 +0800

    Merge origin/master
    Conflicts:
    	app/i18n/zh-CN/checkout.csv

--- app/code/Catalog/Model/Logview.php
+++ app/code/Catalog/Model/Logview.php
@@ -3,6 +3,7 @@
 namespace Seahinet\Catalog\Model;
 
 use Seahinet\Lib\Model\AbstractModel;
+use Seahinet\Catalog\Model\Collection\Logview as Collection;
 
 class Logview extends AbstractModel
 {
@@ -10,6 +11,17 @@ class Logview extends AbstractModel
     protected function construct()
     {
         $this->init('log_view', 'customer_id,product_id', ['customer_id', 'product_id', 'created_at', 'updated_at']);
+    }
+
+    public function getLogview()
+    {
+        if ($this->getId()) {
+            $tracks = new Collection;
+            $tracks->Where(['product_id' => $this->getId()])
+                    ->order('created_at');
+            return $tracks;
+        }
+        return [];
     }
 
 }

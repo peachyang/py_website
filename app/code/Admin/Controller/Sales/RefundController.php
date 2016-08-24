26d1ab22a5000705d784c61d3b6648e2a35bc029
Chase <chasegeng@seahinet.com>
2016-08-24 10:14:18 +0800

    admin/Refund Rights end

--- app/code/Admin/Controller/Sales/RefundController.php
+++ app/code/Admin/Controller/Sales/RefundController.php
@@ -3,6 +3,7 @@
 namespace Seahinet\Admin\Controller\Sales;
 
 use Seahinet\Lib\Controller\AuthActionController;
+use Seahinet\Sales\Model\Rma;
 
 class RefundController extends AuthActionController
 {
@@ -18,6 +19,45 @@ class RefundController extends AuthActionController
     {
         $root = $this->getLayout('admin_refund_view');
         return $root;
+    }
+    
+    public function statusAction()
+    {
+        if ($this->getRequest()->isPost()){
+            $post = $this->getRequest()->getPost();
+            $refund = new Rma;
+            $refund->load($post['id']);
+            $refund->setData('status', $post['status'])->save();
+        }
+        return $this->response(isset($result) ? $result : ['error' => 0, 'message' => []], 'admin/sales_refund/');
+    }
+    
+    
+    public function processingAction()
+    {
+        if ($id = $this->getRequest()->getQuery('id')){
+            $refund = new Rma;
+            $refund->load($id);
+            $refund->setData('status', 0)->save();
+            $result['error'] = 0;
+            $result['message'][] = ['message' => $this->translate('', []), 'level' => 'success'];
+            return $this->response($result, $this->getRequest()->getHeader('HTTP_REFERER'));
+        }
+        return $this->redirectReferer(':ADMIN/sales_refund/');
+    }
+    
+    
+    public function completeAction()
+    {
+        if ($id = $this->getRequest()->getQuery('id')){
+            $refund = new Rma;
+            $refund->load($id);
+            $refund->setData('status', 1)->save();
+            $result['error'] = 0;
+            $result['message'][] = ['message' => $this->translate('', []), 'level' => 'success'];
+            return $this->response($result, $this->getRequest()->getHeader('HTTP_REFERER'));
+        }
+        return $this->redirectReferer(':ADMIN/sales_refund/');
     }
     
 

911ebbfba419e31ed87ebd2c49bd85ea7e2b068d
Stone Sun <stonesun@seahinet.com>
2016-08-25 16:42:20 +0800

    Develop the template prototype of retailer's activity registration.

---
+++ app/code/Retailer/Controller/MarketingController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Seahinet\Retailer\Controller;
+
+use Exception;
+use Seahinet\Retailer\Model\Retailer as Rmodel;
+use Seahinet\Lib\Session\Segment;
+
+/** 
+* Retailer submenu goods marketing controller
+* 
+*/  
+class MarketingController extends AuthActionController
+{
+
+    public function indexAction()
+    {
+        $segment = new Segment('customer');
+        
+        if ($customerId = $segment->get('customer')->getId()) {
+            $customer = new Cmodel;
+            $customer->load($customerId);
+            $root = $this->getLayout('retailer_store_settings');
+            $root->getChild('main', true)->setVariable('customer', $customer);
+            return $root;
+        }
+        return $root;
+    }
+    
+    /** 
+    * releaseAction  
+    * Show release good view
+    * 
+    * @access public 
+    * @return object 
+    */
+    public function activityAction()
+    {
+        $root = $this->getLayout('retailer_activity');
+        return $root;
+    }
+    
+    /** 
+    * viewActivityAction  
+    * Show more information about activity
+    * 
+    * @access public 
+    * @return object 
+    */
+    public function viewActivityAction()
+    {
+        $root = $this->getLayout('retailer_activity_view');
+        return $root;
+    }
+
+}

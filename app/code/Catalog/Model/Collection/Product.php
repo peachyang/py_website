a8b7d54411de6192aa1591486a7e55ac4470ac32
ammon <ammonzhang@seahient.com>
2016-09-07 18:08:27 +0800

    development for products in sales function

--- app/code/Catalog/Model/Collection/Product.php
+++ app/code/Catalog/Model/Collection/Product.php
@@ -20,5 +20,26 @@ class Product extends Collection
         $this->select->where(['store_id' => $store]);
         return $this;
     }
+	
+	/** 
+    * withInSales 
+    * Get product obj in sales
+    * 
+    * @access public
+    * @return object 
+    */ 	
+	public function withInSales($store = null)
+    {
+        if (is_null($store)) {
+            $store = Bootstrap::getStore()->getId();
+        } else if (is_object($store) || is_array($store)) {
+            $store = $store['id'];
+        }
+
+		$this->select->where->nest->isNull('new_end')->or->greaterThanOrEqualTo('new_end', date('Y-m-d H:i:s'))->unnest;
+		$this->select->where->equalTo('store_id',$store);
+		$this->select->where->equalTo('status',1);
+        return $this;
+    }
 
 }

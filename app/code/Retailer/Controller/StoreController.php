851ad4427441999181ffe0e8ee1b74a7258fd285
ammon <ammonzhang@seahient.com>
2016-09-26 16:39:03 +0800

    improvement for store carousel function

--- app/code/Retailer/Controller/StoreController.php
+++ app/code/Retailer/Controller/StoreController.php
@@ -5,8 +5,12 @@ namespace Seahinet\Retailer\Controller;
 use Exception;
 use Seahinet\Retailer\Model\Retailer as Rmodel;
 use Seahinet\Retailer\Model\StoreTemplate;
+use Seahinet\Retailer\Model\StorePicinfo;
 use Seahinet\Retailer\Model\Collection\StoreTemplateCollection;
+use Seahinet\Retailer\Model\Collection\StorePicInfoCollectionCollection;
+use Seahinet\Resource\Model\Collection\Category;
 use Seahinet\Customer\Model\Customer as Cmodel;
+use Seahinet\Resource\Model\Resource as Model;
 use Seahinet\Lib\Session\Segment;
 use Seahinet\Retailer\ViewModel\StoreDecoration as SDViewModel;
 
@@ -218,7 +222,41 @@ class StoreController extends AuthActionController
 		echo json_encode(array('status'=>true,'view'=>$view));
 	}
 	
-	
+	public function decorationUploadAction()
+    {
+        $result = ['error' => 0, 'message' => []];
+        if ($this->getRequest()->isPost()) {
+            $data = $this->getRequest()->getPost();
+            $files = $this->getRequest()->getUploadedFile()['files'];
+            $store = (new Segment('admin'))->get('user')->getStore();
+            $result = $this->validateForm($data);
+            if ($result['error'] === 0) {
+                $categoryCollection = new Category();
+				$categorys =  $categoryCollection->getCategoryByCode($data['resource_category_code']);
+				$category = empty($categorys) ? null : $categorys[0]['id'];		
+                try {
+                    foreach ($files as $file) {
+                        $name = $file->getClientFilename();
+                        $model = new Model();
+                        $model->moveFile($file)
+                                ->setData([
+                                    'store_id' => $store ? $store->getId() : (isset($data['store_id']) && $data['store_id'] ? $data['store_id'] : null),
+                                    'uploaded_name' => $name,
+                                    'file_type' => $file->getClientMediaType(),
+                                    'category_id' => isset($data['category_id']) && $data['category_id'] ? $data['category_id'] : $category
+                                ])->save();
+                        $result['message'][] = ['message' => $this->translate('%s has been uploaded successfully.', [$name], 'resource'), 'level' => 'success'];
+                    }
+                } catch (Exception $e) {
+                    $result['error'] = 1;
+                    $result['message'][] = ['message' => $this->translate($e->getMessage()), 'level' => 'danger'];
+                }
+            }
+        }
+		
+		$result['status'] = $result['error'];
+        return $this->response($result, $this->getRequest()->getHeader('HTTP_REFERER'));
+    }
 	
 
 }

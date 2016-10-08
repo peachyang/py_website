02a6d5833abd79918ad5b5648433a1710291e833
Stone Sun <stonesun@seahinet.com>
2016-10-08 14:16:42 +0800

    Fix the bug of can not add product in retailer center.

---
+++ app/code/Retailer/ViewModel/Catalog/Grid/Product.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Seahinet\Retailer\ViewModel\Catalog\Grid;
+
+use Seahinet\Retailer\ViewModel\Eav\Grid as PGrid;
+use Seahinet\Retailer\Model\Retailer as Retailer;
+use Seahinet\Catalog\Model\Collection\Product as Collection;
+use Seahinet\Lib\Session\Segment;
+
+class Product extends PGrid
+{
+
+    protected $editUrl = '';
+    protected $deleteUrl = '';
+    protected $action = ['getEditAction', 'getDeleteAction'];
+    protected $messAction = ['getExportAction'];
+    protected $translateDomain = 'catalog';
+
+    public function getEditAction($item)
+    {
+        return '<a href="' . $this->getEditUrl() . '?id=' . $item['id'] . '" title="' . $this->translate('Edit') .
+                '"><span class="fa fa-fw fa-file-text-o" aria-hidden="true"></span><span class="sr-only">' .
+                $this->translate('Edit') . '</span></a>';
+    }
+
+    public function getDeleteAction($item)
+    {
+        return $item['status'] ? '' :
+                ('<a href="' . $this->getDeleteUrl() . '" data-method="delete" data-params="id=' . $item['id'] .
+                '&csrf=' . $this->getCsrfKey() . '" title="' . $this->translate('Delete') .
+                '"><span class="fa fa-fw fa-remove" aria-hidden="true"></span><span class="sr-only">' .
+                $this->translate('Delete') . '</span></a>');
+    }
+
+    public function getExportAction()
+    {
+        return '<a href="javascript:void(0);" onclick="var id=\'\';$(\'.grid .table [type=checkbox][value]:checked\').each(function(){id+=$(this).val()+\',\';});location.href=\'' .
+                $this->getAdminUrl('dataflow_product/export/?id=') . '\'+id.replace(/\,$/,\'\');" title="' . $this->translate('Export') .
+                '"><span>' . $this->translate('Export') . '</span></a>';
+    }
+
+    public function getEditUrl()
+    {
+        if ($this->editUrl === '') {
+            $this->editUrl = $this->getAdminUrl(':ADMIN/catalog_product/edit/');
+        }
+        return $this->editUrl;
+    }
+
+    public function getDeleteUrl()
+    {
+        if ($this->deleteUrl === '') {
+            $this->deleteUrl = $this->getAdminUrl(':ADMIN/catalog_product/delete/');
+        }
+        return $this->deleteUrl;
+    }
+
+    protected function prepareColumns($columns = [])
+    {
+        return parent::prepareColumns([
+                    'id' => [
+                        'label' => 'ID',
+                    ]
+        ]);
+    }
+
+    protected function prepareCollection($collection = null)
+    {
+        if (is_null($collection)) {
+            $collection = new Collection;
+            $user = (new Segment('customer'))->get('customer');
+            $retailer = new Retailer;
+            $retailer->load($user->getId(),'customer_id');
+            if ($retailer['store_id']) {
+                $collection->where(['store_id' => $retailer['store_id']]);
+            }
+        }
+        return parent::prepareCollection($collection);
+    }
+
+}
---
+++ app/code/Retailer/ViewModel/Eav/Grid.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Seahinet\Retailer\ViewModel\Eav;
+
+use Seahinet\Admin\ViewModel\Grid as PGrid;
+use Seahinet\Retailer\Model\Retailer as Retailer;
+use Seahinet\Lib\Bootstrap;
+use Seahinet\Lib\Model\Eav\Attribute as AttributeModel;
+use Seahinet\Lib\Model\Collection\Eav\Attribute;
+use Seahinet\Lib\Session\Segment;
+use Seahinet\Lib\Source\Store;
+
+abstract class Grid extends PGrid
+{
+
+    protected function prepareColumns($columns = [])
+    {
+        $attributes = new Attribute;
+        $languageId = Bootstrap::getLanguage()->getId();
+        $collection = $this->getVariable('collection');
+        $attributes->withLabel($languageId)
+                ->join('eav_entity_type', 'eav_entity_type.id=eav_attribute.type_id', [], 'right')
+                ->where(['eav_entity_type.code' => $collection::ENTITY_TYPE])
+                ->where('(filterable=1 OR sortable=1)');
+        //$user = (new Segment('admin'))->get('user');
+        $user = (new Segment('customer'))->get('customer');
+        $retailer = new Retailer;
+        $retailer->load($user->getId(),'customer_id');
+        if (empty($columns)) {
+            $columns = [
+                'id' => [
+                    'label' => 'ID',
+                ],
+                'store_id' => ($retailer['store_id'] ? [
+                    'type' => 'hidden',
+                    'value' => $retailer['store_id'],
+                    'use4sort' => false,
+                    'use4filter' => false
+                        ] : [
+                    'type' => 'select',
+                    'options' => (new Store)->getSourceArray(),
+                    'label' => 'Store'
+                        ])
+            ];
+        }
+        foreach ($attributes as $attribute) {
+            $columns[$attribute['code']] = [
+                'label' => $attribute['label'],
+                'type' => $attribute['input'],
+                'class' => $attribute['validation'],
+                'view_model' => $attribute['view_model'],
+                'use4sort' => $attribute['sortable'],
+                'use4filter' => $attribute['filterable']
+            ];
+            if (in_array($attribute['input'], ['select', 'radio', 'checkbox', 'multiselect'])) {
+                $columns[$attribute['code']]['options'] = (new AttributeModel($attribute))->getOptions($languageId);
+            }
+        }
+        $columns['status'] = [
+            'type' => 'select',
+            'label' => 'Status',
+            'options' => [
+                1 => 'Enabled',
+                0 => 'Disabled'
+            ]
+        ];
+        return $columns;
+    }
+
+    protected function prepareCollection($collection = null)
+    {
+        $user = (new Segment('customer'))->get('customer');
+        $retailer = new Retailer;
+        $retailer->load($user->getId(),'customer_id');
+        if ($retailer['store_id']) {
+            $collection->where(['store_id' => $retailer['store_id']]);
+        }
+        return parent::prepareCollection($collection);
+    }
+
+}

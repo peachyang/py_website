6f4183315b93d1ea682fe083af6862797dc84528
peach <peach@DESKTOP-RD2AI69>
2016-10-28 09:39:00 +0800

    Merge origin/master
    Conflicts:
    	pub/theme/default/frontend/css/styles.css

--- app/code/Retailer/Controller/StoreController.php
+++ app/code/Retailer/Controller/StoreController.php
@@ -71,6 +71,15 @@ class StoreController extends AuthActionController
         $root = $this->getLayout('freight_setting');
         return $root;
     }
+    /**
+     * freightAction
+     * show Freight setting
+     */
+    public function freightAction()
+    {
+        $root = $this->getLayout('freight_setting');
+        return $root;
+    }
 
     public function saveAction()
     {

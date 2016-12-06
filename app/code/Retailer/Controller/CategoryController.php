563d12e02dce4ed77068fa086195c2717f7b6ebb
peach <peach@DESKTOP-RD2AI69>
2016-12-06 17:46:38 +0800

    refund

--- app/code/Retailer/Controller/CategoryController.php
+++ app/code/Retailer/Controller/CategoryController.php
@@ -62,6 +62,8 @@ class CategoryController extends AuthActionController
 
     public function removeAction()
     {
+        $data = $this->getRequest()->isGet() ? $this->getRequest()->getQuery() : $this->getRequest()->getPost();
+        $result = $this->validateForm($data);
         if ($this->getRequest()->isDelete()) {
             $data = $this->getRequest()->getPost();
             $result = $this->validateForm($data);

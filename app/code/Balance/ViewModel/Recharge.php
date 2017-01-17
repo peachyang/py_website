96966dd436dfe69674f30264d38f7c387b765bcb
peach <peach@DESKTOP-RD2AI69>
2017-01-17 17:37:18 +0800

    Merge origin/master
    Conflicts:
    	app/code/Balance/Controller/StatementController.php

--- app/code/Balance/ViewModel/Recharge.php
+++ app/code/Balance/ViewModel/Recharge.php
@@ -2,10 +2,8 @@
 
 namespace Seahinet\Balance\ViewModel;
 
-use Seahinet\Catalog\ViewModel\Product\View;
 use Seahinet\Customer\ViewModel\Account;
 use Seahinet\Catalog\Model\Product;
-use Seahinet\Lib\Source\SourceInterface;
 
 class Recharge extends Account
 {
